244515216efefc6d668e06f6e624e564e33cda6c
Chandler Williams <mchandlerwilliams@gmail.com>
2017-02-25 20:52:55 -0600

    adding hamming functions for js and python tests

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -5,3 +5,18 @@
 
 
 module.exports = Hamming;
+
+function Hamming()
+{
+	//compare the strings in the loop.  If the char in str1 is not the same as in str2, increment counter
+	this.compute = function(str1, str2){
+		if (str1.length !== str2.length) throw 'DNA strands must be of equal length.'
+		var count = 0;
+		for (i = 0; i < str1.length; i++) {
+		  if (str1[i] != str2[i]) {
+			count = count + 1;
+		  }
+		}
+		return count;
+	}
+}
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,11 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
+def compute(str1, str2):
+    if len(str1) == len(str2): #string must be the same length
+        return sum(x != y for x, y in zip(str1, str2)) #iterate over and compare the chars in each string; return count of nonequal chars
+    else:
+        raise ValueError('The string parameter lengths are not equal')
     pass
+
+ 
